169a6902f2fa6bc4eb1aae193befb5641ffff552
Aarambha Bhandari <arambha21@gmail.com>
2025-01-29 21:59:09 +0000

    Update URL configuration for static files and add user-related URLs

--- hris_project/urls.py
+++ hris_project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from users import views as user_views
+from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
