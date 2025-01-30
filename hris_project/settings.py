cd90122b1050f06d567cb728e2ddb2cb59927c8c
Aarambha Bhandari <arambha21@gmail.com>
2025-01-30 02:59:06 +0000

    changed

--- hris_project/settings.py
+++ hris_project/settings.py
@@ -14,6 +14,12 @@ from pathlib import Path
 import os.path  
 import sys
 
+import mimetypes
+
+
+mimetypes.add_type("text/css", ".css", True)
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
